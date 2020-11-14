7f94aabdca354ea287826c35f4597890d1faa309
Piradon-lab <71110711+Piradon-lab@users.noreply.github.com>
2020-11-14 16:17:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-    <table width="600" border="1">
+   <table class="table table-dark table-striped">
         <tr>
             <th width="100">
                 <div align="center">Name</div>
